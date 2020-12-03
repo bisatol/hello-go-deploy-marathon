3962a3f19d7c73b2c67cf2f0c822a221e4571dc5
Jeff DeCola (TK1-PC Bash on Ubuntu on Windows) <jeff@keeperlabs.com>
2020-12-02 21:24:01 -0800

    update ci

--- ci/scripts/unit-tests.sh
+++ ci/scripts/unit-tests.sh
@@ -17,9 +17,15 @@ else
     echo " "
 fi
 
+echo "GOAL ----------------------------------------------------------------------------------"
+echo " "
+
 echo "The goal is to set up a go src/github.com/JeffDeCola/hello-go-deploy-marathon directory"
 echo "Then tests will be run in that directory"
 echo "Test coverage results, text_coverage.txt, will be moved to /coverage-results directory"
+echo " "
+
+echo "CHECK THINGS --------------------------------------------------------------------------"
 echo " "
 
 echo "At start, you should be in a /tmp/build/xxxxx directory with two folders:"
@@ -34,13 +40,19 @@ echo "List whats in the current directory"
 ls -la
 echo " "
 
+echo "SETUP GO ------------------------------------------------------------------------------"
+echo " "
+
 echo "Setup the GOPATH based on current directory"
+echo "export GOPATH=\$PWD"
 export GOPATH=$PWD
 echo " "
 
 echo "Now we must move our code from the current directory ./hello-go-deploy-marathon to" 
 echo "$GOPATH/src/github.com/JeffDeCola/hello-go-deploy-marathon"
+echo "mkdir -p src/github.com/JeffDeCola/"
 mkdir -p src/github.com/JeffDeCola/
+echo "cp -R ./hello-go-deploy-marathon src/github.com/JeffDeCola/."
 cp -R ./hello-go-deploy-marathon src/github.com/JeffDeCola/.
 echo " "
 
@@ -57,14 +69,23 @@ echo "ls -la"
 ls -la
 echo " "
 
-echo "Run go test -cover"
+echo "RUN TESTS -----------------------------------------------------------------------------"
+echo " "
+
+echo "Run go tests"
+echo "go test -cover ./... | tee test/test_coverage.txt"
 echo "   -cover shows the percentage coverage"
 echo "   Put results in /test/test_coverage.txt file"
 go test -cover ./... | tee test/test_coverage.txt
+# echo "Placeholder to run go tests for my-go-examples" | tee testcode/test_coverage.txt
 echo " "
 
 echo "Clean test_coverage.txt file - add some whitespace to the begining of each line"
+echo "sed -i -e 's/^/     /' test/test_coverage.txt"
 sed -i -e 's/^/     /' test/test_coverage.txt
+echo " "
+
+echo "MOVE TEST COVERAGE FILE ---------------------------------------------------------------"
 echo " "
 
 echo "The test_coverage.txt file will be used by the concourse pipeline to send to slack"
